1f61c7c430ede88375449ceb7cdae16633dae73b
Glitch (file-wine) <none>
2019-07-15 07:00:24 +0000

    🍽📹 Checkpoint ./cache.js:3443349/956 ./package.json:3443349/1283

--- cache.js
+++ cache.js
@@ -1,4 +1,5 @@
 var config = require('./config.js')
+var request = require('request')
 var cache = {
     domains: []
 }
@@ -8,18 +9,31 @@ var cache = {
     // }
 var initCache = function(){
   for(let domain of config.domains){
-    cache.domains.push({url:domain,last_checked: "0"})
-    console.log(domain)
-    
+    cache.domains.push(
+      {
+      url:domain,
+      last_checked: "0",
+      working: null
+      }
+    )
   }
-  console.log(cache)
 }
 var setCache = function() {
 
     for (let _domain in cache.domains) {
         let domain = cache.domains[_domain];
         cache.domains[_domain].last_checked = Date.now();
-    }
+      request('https://' + cache.domains[_domain].url).on('response', function(response) {
+        if(response.statusCode === 200){
+          cache.domains[_domain].working = true
+        }else{
+                    cache.domains[_domain].working = false
+
+        }
+    console.log(response.statusCode) // <--- Here 200
+})
+      
+        }
         return cache
 
 }
@@ -27,11 +41,8 @@ var checkCache = function(callback) {
     for (let _domain in cache.domains) {
         let domain = cache.domains[_domain];
         if (Date.now() - domain.last_checked > 300000) {
-
             return callback(setCache())
         }else{
-                    console.log('cache!')
-
             return callback(cache)
         }
     }
