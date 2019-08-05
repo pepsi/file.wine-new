cd91bdfc7206938a0a88ce36cd05c4c1d2033b5e
Glitch (file-wine) <none>
2019-08-05 19:05:36 +0000

    🍱🏜 Checkpoint ./cache.js:3443349/139

--- cache.js
+++ cache.js
@@ -12,22 +12,25 @@ var initCache = function() {
         })
     }
 }
-var setCache = function() {
+var setCache = function(callback) {
 
     for (let _domain in cache.domains) {
         let domain = cache.domains[_domain];
         cache.domains[_domain].last_checked = Date.now();
-        request('https://' + cache.domains[_domain].url + "/ping").on('response', function(response) {
+        request('https://' + cache.domains[_domain].url + "/").on('response', function(response) {
           cache.domains[_domain].working = response.statusCode === 200
         })
     }
-    return cache
+    return callback(cache)
 }
 var checkCache = function(callback) {
     for (let _domain in cache.domains) {
         let domain = cache.domains[_domain];
         if (Date.now() - domain.last_checked > 300000) {
-            return callback(setCache())
+            // return callback(setCache())
+          setCache((c) => {
+            return callback(c)
+          })
         } else {
             return callback(cache)
         }
