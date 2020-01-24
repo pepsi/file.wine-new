bdd028ee4b7589310f04e66180c822ff25ba9400
Glitch (file-wine) <none>
2020-01-24 10:35:40 -0500

    Fixed alot of things or something i dont remember

--- server.js
+++ server.js
@@ -1,45 +1,43 @@
 const express = require('express');
 const path = require('path');
-const fs =require('fs')
+const fs = require('fs')
 const config = require('./config.js');
 const cache = require('./cache.js');
 var fileUpload = require('express-fileupload');
 const app = express();
+
 function makeid() {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
-  
-  for ( var i = 0; i < 5; i++ ) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+
+  for (var i = 0; i < 5; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
-  blacklist = ["up","up.php","u","u.php","upload","upload.php","","domains","public","assets"]
-  if(blacklist.includes(result)){
+  blacklist = ["up", "up.php", "u", "u.php", "upload", "upload.php", "", "domains", "public", "assets"]
+  if (blacklist.includes(result)) {
     result = makeid()
   }
   return result;
 }
 
 app.use(fileUpload({
-    safeFileNames: false,
-    preserveExtension: true,
-    limits: {
-        fileSize: config.fileSizeLimit
-    }
+  safeFileNames: false,
+  preserveExtension: true,
+  limits: {
+    fileSize: config.fileSizeLimit
+  }
 }));
 
 app.set('view engine', 'pug')
 app.use('/static', express.static(path.join(__dirname + '/public')));
-app.get('/sharex', (req,res) => { 
-_config = `{
+app.get('/sharex', (req, res) => {
+  _config = `{
   "Version": "12.4.1",
   "DestinationType": "ImageUploader, TextUploader, FileUploader",
   "RequestMethod": "POST",
-  "RequestURL": "http://${req.host}/i/up.php",
+  "RequestURL": "http://${req.host}/up.php",
   "Body": "MultipartFormData",
-  "Arguments": {
-    "secret": "NotII"
-  },
   "FileFormName": "sharex"
   }`
   res.set('Content-Disposition', 'attachment;filename=' + req.host + '.sxcu')
@@ -48,16 +46,13 @@ _config = `{
 })
 app.get('/', (req, res) => {
   res.render('index', {})
-  // res.sendFile(__dirname + '/public/index.html')
 })
 app.get('/domains', (req, res) => {
   cache.checkCache((c) => {
     res.render('domains', {
       cache: c
     })
-    // res.send(c)
   })
-
 })
 app.get('/assets/styles.css', (req, res) => {
   res.sendFile(__dirname + '/public/style.css')
@@ -70,22 +65,19 @@ app.get('/api/v1/status', (req, res) => {
     res.send(c)
   })
 })
-app.get('/domains', (req, res) => {
+app.get('/:img', handleGrabImage)
+app.get('/i/:img', handleGrabImage)
 
-
-
-})
 app.post('/up.php', (req, res) => {
-  return  handleUpload(req, res)
+  return handleUpload(req, res)
 })
 app.post('/upload.php', (req, res) => {
   return handleUpload(req, res)
 })
-app.post('/up',  (req, res) => {
+app.post('/up', (req, res) => {
   return handleUpload(req, res)
 })
 app.post('/upload', (req, res) => {
-  // res.send('test')
   return handleUpload(req, res)
 })
 app.post('/u.php', (req, res) => {
@@ -94,19 +86,16 @@ app.post('/u.php', (req, res) => {
 app.post('/u', (req, res) => {
   return handleUpload(req, res)
 })
-
-
 app.post('/i/up.php', (req, res) => {
-  return  handleUpload(req, res)
+  return handleUpload(req, res)
 })
 app.post('/i/upload.php', (req, res) => {
   return handleUpload(req, res)
 })
-app.post('/i/up',  (req, res) => {
+app.post('/i/up', (req, res) => {
   return handleUpload(req, res)
 })
 app.post('/i/upload', (req, res) => {
-  // res.send('test')
   return handleUpload(req, res)
 })
 app.post('/i/u.php', (req, res) => {
@@ -115,35 +104,37 @@ app.post('/i/u.php', (req, res) => {
 app.post('/i/u', (req, res) => {
   return handleUpload(req, res)
 })
-
-
-app.get('/:img',handleGrabImage)
-app.get('/i/:img', handleGrabImage)
-
-
-function handleGrabImage(req, res){
-  file = __dirname  + path.sep + "images" + path.sep + req.params.img
-  console.log('grabbing ' + file)
+function handleGrabImage(req, res) {
+  file = __dirname + path.sep + "images" + path.sep + req.params.img
+  if(fs.existsSync(file)){
   res.sendFile(file)
-  // fs.readFile(file, (err, data) => {
-  //   if(err){
-  //     console.log(err)
-  //     res.send('an error has occured, maybe this file doesnt exist?')
-  //   }else{
-  //   res.send(data)
-  // }
-  // })
-}
-function handleUpload(req, res) {
-
-  // console.log(req.files)
-  const id = makeid();
-  extension = req.files.sharex.name.split('.')[1] || ''
-  if(['bat','exe'].includes(extension)){
-
+  }else{
+    res.render('404', {
+      reason: "Image not found. Did you type the URL correctly?"
+    })
   }
-  req.files.sharex.mv(__dirname + '/images/' + id + '.' + extension)
-  res.send('https://' + req.hostname + '/' + id + '.' + extension)
+}
+app.all('*',(req,res) => {
+  res.render('404', {
+    reason: "Page not found. Did you type the URL correctly?"
+  })
+})
+function handleUpload(req, res) {
+  const id = makeid();
+  if(req.files){
+    if(req.files.sharex){
+      extension = req.files.sharex.name.split('.')[1] || ''
+      if (['bat', 'exe'].includes(extension)) {
+    
+      }
+      req.files.sharex.mv(__dirname + '/images/' + id + '.' + extension)
+      res.send('https://' + req.hostname + '/' + id + '.' + extension)
+    }else{
+      res.send("No file named sharex was uploaded.")
+    }
+  }else{
+    res.send("No file uploaded.")
+  }
 }
 const listener = app.listen(4004, function () {
   cache.initCache()
